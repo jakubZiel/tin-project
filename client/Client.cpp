8453ebc28eddbfb4350a45f0f3b1062e1693b921
osapiech <olga.sapiechowska@gmail.com>
2022-01-22 17:26:14 +0100

    Sending/receiving mode switch

--- client/Client.cpp
+++ client/Client.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-Client::Client(char * request) {
+Client::Client(char * request, char * mode) {
     tv.tv_sec = 3;
     tv.tv_usec = 0;
 
@@ -14,6 +14,10 @@ Client::Client(char * request) {
 
     request_buffer = vector<char>(50);
     strcpy(request_buffer.data(), request);
+
+    client_mode = vector<char>(50);
+    strcpy(client_mode.data(), mode);
+
     is_last = vector<char>(50);
 }
 
@@ -25,38 +29,42 @@ int Client::run() {
     while (true) {
         sleep(1);
 
-        if (sendto(client_socket, request_buffer.data(), request_buffer.size(), 0, (sockaddr * ) & server_address,
-                   sizeof(server_address)) <= 0) {
-            cout << "Send / Err :" << errno << endl;
-            break;
-        } else {
-            counter++;
-            cout << "Wrote " << counter << endl;
-        }
-        socklen_t server_address_len = sizeof(server_address);
-
-        fd_set rfds;
-        FD_ZERO(&rfds);
-        FD_SET(client_socket, &rfds);
-
-        if (select(client_socket + 1, &rfds, NULL, NULL, &tv)) {
-            if (recvfrom(client_socket, is_last.data(), is_last.size(), 0, (sockaddr * ) & server_address,
-                         &server_address_len) <= 0) {
-
-                switch (errno) {
-                    case CONNECTION_REFUSED:
-                        cout << "Receive / Connection refused" << endl;
-                        break;
-                    case TIMEOUT:
-                        cout << "Receive / Timeout" << endl;
-                        break;
-                    default:
-                        cout << "Receive / Unknown error" << endl;
-                }
+        if (strcmp(client_mode.data(), "1") == 0) {
+            if (sendto(client_socket, request_buffer.data(), request_buffer.size(), 0, (sockaddr *) &server_address,
+                       sizeof(server_address)) <= 0) {
+                cout << "Send / Err :" << errno << endl;
                 break;
+            } else {
+                counter++;
+                cout << "Wrote " << counter << endl;
             }
         }
-        cout << "Message for :" << is_last.data() << endl;
+
+        if (strcmp(client_mode.data(), "2") == 0) {
+            socklen_t server_address_len = sizeof(server_address);
+            fd_set rfds;
+            FD_ZERO(&rfds);
+            FD_SET(client_socket, &rfds);
+
+            if (select(client_socket+1, &rfds, NULL, NULL, &tv)) {
+                if (recvfrom(client_socket, is_last.data(), is_last.size(), 0, (sockaddr *) &server_address,
+                             &server_address_len) <= 0) {
+
+                    switch (errno) {
+                        case CONNECTION_REFUSED:
+                            cout << "Receive / Connection refused" << endl;
+                            break;
+                        case TIMEOUT:
+                            cout << "Receive / Timeout" << endl;
+                            break;
+                        default:
+                            cout << "Receive / Unknown error" << endl;
+                    }
+                    break;
+                }
+                cout << "Message for :" << is_last.data() << endl;
+            }
+        }
     }
 
     cout << "Client :" << request_buffer.data() << " Sent :" << counter;
@@ -86,6 +94,6 @@ int main(int argc, char** argv) {
         return -2;
     }
 
-    Client client(argv[1]);
+    Client client(argv[1], argv[2]);
     client.run();
 }
--- include/Client.h
+++ include/Client.h
@@ -11,11 +11,12 @@ private:
     sockaddr_in server_address{};
     int client_socket{};
 
+    std::vector<char> client_mode;
     std::vector<char> request_buffer;
     std::vector<char> is_last;
 
 public:
-    Client(char * request);
+    Client(char * request, char * mode);
 
     int init_socket(int protocol_type);
     sockaddr_in inet_association(sa_family_t in_family, in_port_t port, in_addr_t address);
