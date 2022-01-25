5e33c8c418bf82a94e3be7c77902c362b24c2ac3
jskonecz <j.skoneczna@stud.elka.pw.edu.pl>
2022-01-25 01:19:41 +0100

    Fix setting max users number in channel

--- server/AdminServer.cpp
+++ server/AdminServer.cpp
@@ -262,7 +262,7 @@ void AdminServer::prepare_command_table() {
     command_table["ban"] = BAN;
     command_table["unban"] = UNBAN;
     command_table["get_banned_users"] = USERS;
-    command_table["set_max_users"] = SET_MAX_SIZE;
+    command_table["max_users"] = SET_MAX_SIZE;
     command_table["set_privacy"] = SET_PRIVACY;
 }
 
